7159702a7b8a46498bb6935f597eda30d3510a17
John Hinojosa <hinojohn@ymail.com>
2021-05-24 22:19:39 -0500

    grasping at test straws, attempts to fix heroku continue

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-// Exporting an object containing all models
+// // Exporting an object containing all models
 
-module.exports = {
-  Workout: require("./Workout"),
-};
+// module.exports = {
+//   Workout: require("./Workout"),
+// };
