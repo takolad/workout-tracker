530f06b15d8f0b8bec223b402664bca0f9282488
John Hinojosa <hinojohn@ymail.com>
2021-05-24 21:38:25 -0500

    fixed? Workout require

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models");
+const Workout = require("../models/Workout");
 const path = require("path");
 
 router.get("/", (req, res) => {
